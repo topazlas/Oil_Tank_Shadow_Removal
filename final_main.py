fdce7fb33e504d85ed37385602b13ebbf2d0078a
topazlas <yimkyeongbin@gmail.com>
2023-12-29 11:10:53 +0900

    update

--- final_main.py
+++ final_main.py
@@ -29,42 +29,60 @@ from PIL import Image
 
 if __name__ == '__main__':
     # -------------------------------- get mask main.py --------------------------------------
+
+
+
     # YOLO 모델 로드
-    yolo_model = YOLO("models/Oil_Tank_Shadow_Detect_Model_v3.pt")
+    model = YOLO("models/Drone_Shadow_Detect_v4.pt")
 
     # 추론할 이미지가 있는 폴더 경로 지정
-    image_folder = "Dataset/test/test_A/"  # 원하는 이미지 폴더 경로로 수정
+    image_folder = "Dataset/test/original/"  # 원하는 이미지 폴더 경로로 수정
 
     # 결과 이미지를 저장할 폴더 경로 지정
     output_folder = "Dataset/test/test_B/"  # 원하는 결과 이미지 폴더 경로로 수정
 
-    # 이미지 폴더 내의 모든 이미지 파일에 대해 추론 및 저장
+    # 원하는 이미지 사이즈로 설정
+    img_size = 1280  # 원하는 이미지 사이즈로 수정
+
+    # 리사이즈된 원본 이미지를 저장할 폴더 경로 지정
+    resized_folder = "Dataset/test/test_A/"  # 새로운 폴더 경로로 수정
+
+    # 이미지 폴더 내의 모든 이미지 파일에 대해 추론 및 마스크 이미지 저장
     for filename in os.listdir(image_folder):
-        if filename.endswith(".jpg") or filename.endswith(".png"):
+        if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".JPG"):
             # 이미지 파일 경로
             image_path = os.path.join(image_folder, filename)
 
             # 이미지 추론
-            results = yolo_model.predict(image_path)
+            results = model.predict(image_path, imgsz=img_size)
             result = results[0]
             masks = result.masks
 
             # 모든 마스크 이미지를 하나로 병합
             combined_mask = sum(mask.data[0].cpu().numpy() for mask in masks)
 
-            # 이미지로 변환
+            # 마스크 이미지로 변환
             combined_mask_img = Image.fromarray(combined_mask.astype('uint8') * 255, "L")
 
-            # 결과 이미지 파일명 지정 (순차적으로 번호 붙임)
-            output_filename = f"{filename.split('.')[0]}.png"
+            # 결과 마스크 이미지 파일명 지정 (순차적으로 번호 붙임)
+            mask_filename = f"{filename.split('.')[0]}.png"
+            mask_output_path = os.path.join(output_folder, mask_filename)
 
-            # 결과 이미지 파일 경로
-            output_path = os.path.join(output_folder, output_filename)
+            # 결과 마스크 이미지 저장
+            combined_mask_img.save(mask_output_path, format="PNG")
 
-            # 결과 이미지 저장
-            combined_mask_img.save(output_path, format="PNG")
+            # 원본 이미지 리사이즈
+            original_image = Image.open(image_path)
+            original_image_resized = original_image.resize(combined_mask_img.size, Image.LANCZOS)
 
-#-------------------------------------------------------------------------------------------
+            # 리사이즈된 원본 이미지 파일명 지정 (순차적으로 번호 붙임)
+            resized_filename = f"{filename.split('.')[0]}.png"
+            resized_path = os.path.join(resized_folder, resized_filename)
+
+            # 리사이즈된 원본 이미지 저장
+            original_image_resized.save(resized_path, format="PNG")
+
+    #-------------------------------------------------------------------------------------------
 
     parser = argparse.ArgumentParser(description='RGB denoising evaluation on the validation set of SIDD')
     parser.add_argument('--input_dir', default='Dataset/test/',
